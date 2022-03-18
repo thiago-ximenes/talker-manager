ad414151f2c3c52a267834a580477039e14fc6f9
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-03-18 14:11:08 -0300

    fiz requirimint 1 when there was not a talker response

--- middlewares/showTalkers.js
+++ middlewares/showTalkers.js
@@ -3,6 +3,9 @@ const getTalkers = require('../services/getTalkers');
 async function showTalkers(req, res) {
   try {
     const talkers = await getTalkers();
+    if (talkers.length === 0) {
+      res.status(200).json([]);
+    }
     return res.status(200).json(talkers);
   } catch (err) {
     console.log(err);
