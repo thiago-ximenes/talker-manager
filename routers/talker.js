ce3716741f0d8facb239b860bef5d206cd6a3e70
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-03-18 15:12:24 -0300

    pass requiriment 2

--- routers/talker.js
+++ routers/talker.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const showTalkers = require('../middlewares/showTalkers');
+const findTalkerById = require('../middlewares/findTalkerById');
 
 router.get('/', showTalkers);
+
+router.get('/:id', findTalkerById);
 
 module.exports = router;
